8add7dba7ee341065624647fbd56a1b9e014ea77
junio <junio@uGanja>
2026-04-12 15:45:28 +0200

    I tested my codes JUnit

---
+++ src/test/java/com/mycompany/quickchat/TestQuicKChat.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.quickchat;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author A.Ntuli
+ */
+public class TestQuicKChat { 
+    
+      // checking username rule
+    @Test
+    public void testUserNameOk() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Pass123!", "+27649963977");
+        assertTrue(l.checkUserName());
+    }
+    
+    @Test
+    public void testUserNameFail() {
+        Login l = new Login("Andile", "Ntuli", "andile", "Pass123!", "+27649963977");
+        assertFalse(l.checkUserName());
+    }
+    
+    // password tests
+    @Test
+    public void testPasswordGood() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Pass123!", "+27649963977");
+        assertTrue(l.checkPasswordComplexity());
+    }
+    
+    @Test
+    public void testPasswordNoNum() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Password!", "+27649963977");
+        assertFalse(l.checkPasswordComplexity());
+    }
+    
+    @Test
+    public void testPasswordNoSpecial() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Password1", "+27649963977");
+        assertFalse(l.checkPasswordComplexity());
+    }
+    
+    // cell number tests
+    @Test
+    public void testCellGood() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Pass123!", "+27649963977");
+        assertTrue(l.checkCellPhoneNumber());
+    }
+    
+    @Test
+    public void testCellBad() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Pass123!", "0649963977");
+        assertFalse(l.checkCellPhoneNumber());
+    }
+    
+    // login tests
+    @Test
+    public void testLoginOk() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Pass123!", "+27649963977");
+        assertTrue(l.loginUser("an_", "Pass123!"));
+    }
+    
+    @Test
+    public void testLoginWrong() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Pass123!", "+27649963977");
+        assertFalse(l.loginUser("wrong", "Pass123!"));
+    }
+    
+    // login message tests
+    @Test
+    public void testLoginMsgOk() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Pass123!", "+27649963977");
+        String msg = l.returnLoginStatus(true);
+        assertTrue(msg.contains("Welcome"));
+    }
+    
+    @Test
+    public void testLoginMsgFail() {
+        Login l = new Login("Andile", "Ntuli", "an_", "Pass123!", "+27649963977");
+        String msg = l.returnLoginStatus(false);
+        assertEquals("Username or password incorrect, please try again.", msg);
+    }
+}   
+        
+    
+    
+

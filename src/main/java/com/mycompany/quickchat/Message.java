d9e52069a477d9e25e93cace5b428fbebb246c59
junio <junio@uGanja>
2026-06-13 13:17:11 +0200

    declared public static string findByID ( String id){

--- src/main/java/com/mycompany/quickchat/Message.java
+++ src/main/java/com/mycompany/quickchat/Message.java
@@ -24,7 +24,7 @@ public class Message {
     private static JSONArray ids = new JSONArray();
 
     // Maximums
-    private static final int MAX_MESSAGES = 100; 
+    private static final int MAX_MESSAGES = 100;
 
     public Message(String id, String recipient, String text) {
         this.id = id;
@@ -97,7 +97,7 @@ public class Message {
     }
 
     // Search by ID across both sent and stored
-    public static String findByID(String id) {
+    public static String findByID(String id) { 
         for (Object o : stored) {
             JSONObject m = (JSONObject) o;
             if (id.equals(m.get("id"))) {
